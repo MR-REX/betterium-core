9df2e3e1946fec4f05b69b2b69227fb876f19c11
MR.REX <mrrex.main@gmail.com>
2025-11-11 15:05:14 +0300

    Update module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,14 +4,26 @@ module ru.mrrex.betterium.core {
     requires com.fasterxml.jackson.databind;
     requires java.net.http;
 
+    exports ru.mrrex.betterium.core.artifact;
+    exports ru.mrrex.betterium.core.artifact.implementation;
+
     exports ru.mrrex.betterium.core.client;
     exports ru.mrrex.betterium.core.client.config;
+
+    exports ru.mrrex.betterium.core.condition;
+    exports ru.mrrex.betterium.core.condition.exception;
+    exports ru.mrrex.betterium.core.condition.validator;
+
+    exports ru.mrrex.betterium.core.download.downloader;
+    exports ru.mrrex.betterium.core.download.downloader.http;
+    exports ru.mrrex.betterium.core.download.exception;
+    exports ru.mrrex.betterium.core.download.listener;
+    exports ru.mrrex.betterium.core.download.stream;
 
     exports ru.mrrex.betterium.core.library;
     exports ru.mrrex.betterium.core.library.implementation;
 
-    exports ru.mrrex.betterium.core.artifact;
-    exports ru.mrrex.betterium.core.artifact.implementation;
+    exports ru.mrrex.betterium.core.preprocessor;
 
     exports ru.mrrex.betterium.core.resource;
 
