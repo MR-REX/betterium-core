1c80d5e2979641e0662c91dccc47bfeb41cca623
MR.REX <mrrex.main@gmail.com>
2025-11-14 02:32:25 +0300

    Improvements for ChecksumAlgorithm enum

--- src/main/java/ru/mrrex/betterium/core/checksum/ChecksumAlgorithm.java
+++ src/main/java/ru/mrrex/betterium/core/checksum/ChecksumAlgorithm.java
@@ -3,6 +3,9 @@ package ru.mrrex.betterium.core.checksum;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
+
 public enum ChecksumAlgorithm {
 
     CRC32,
@@ -20,18 +23,23 @@ public enum ChecksumAlgorithm {
     }
 
     public static ChecksumAlgorithm findByName(String algorithmName) {
-        for (ChecksumAlgorithm algorithm : ChecksumAlgorithm.values())
+        Objects.requireNonNull(algorithmName, "Checksum algorithm name must not be null");
+
+        if (algorithmName.isBlank())
+            return null;
+
+        for (ChecksumAlgorithm algorithm : values())
             if (algorithmName.equalsIgnoreCase(algorithm.name()))
                 return algorithm;
 
         return null;
     }
 
-    public static ChecksumAlgorithm getByName(String algorithmName) {
+    public static ChecksumAlgorithm getByName(String algorithmName) throws NoSuchAlgorithmException {
         ChecksumAlgorithm foundAlgorithm = findByName(algorithmName);
 
         if (foundAlgorithm == null)
-            throw new IllegalArgumentException("No such checksum algorithm: " + algorithmName);
+            throw new NoSuchAlgorithmException("No such checksum algorithm: " + algorithmName);
 
         return foundAlgorithm;
     }
